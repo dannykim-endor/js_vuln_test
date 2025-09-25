5ddea2ce54ecfab6e51c0c2e63577dbea912a2b9
Danny Kim <danny@endor.ai>
2025-09-25 13:15:52 -0700

    debug commit

--- mongoose_cast.js
+++ mongoose_cast.js
@@ -3,5 +3,11 @@
 
 var mg = require('mongoose/lib/cast')
 var mg3 = require('mongoose/lib/model')
+var Schema = require('mongoose/lib/schema')
+
 console.log(mg('y',{}));
-mg3.mapReduce()
+
+// Call Schema.interpretAsType function
+// Parameters: path (string), obj (object), options (object)
+var result = Schema.interpretAsType('testPath', { type: String }, { typeKey: 'type' });
+console.log('interpretAsType result:', result);
